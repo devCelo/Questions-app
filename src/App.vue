89bee19b891c7a3abb9567d0bb8bbd7579642d05
devCelo <marcelosoaresmkt@gmail.com>
2019-05-08 10:26:59 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="app">
+    <Header />
+    <b-container>
+      <b-row>
+        <b-col>
+          <questionBox />
+        </b-col>
+      </b-row>
+    </b-container>
+  </div>
+</template>
+
+<script>
+import Header from './components/header.vue'
+import questionBox from './components/questionBox.vue'
+
+export default {
+  name: 'app',
+  components: {
+    Header,
+    questionBox
+  },
+mounted: function() {
+  fetch('https://opentdb.com/api.php?amount=10'), {
+    method: "get"
+  }
+},
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
---
+++ src/components/header.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+  <b-nav fill class="mb-5">
+    <b-nav-item active>Active</b-nav-item>
+    <b-nav-item>Link</b-nav-item>
+    <b-nav-item>Another Link</b-nav-item>
+    <b-nav-item disabled>Disabled</b-nav-item>
+  </b-nav>
+</div>
+</template>
